67c8e5c42c8efa29d0e969c5c939cc6ba2ca0637
victor mgbeahurike <victormgbeahurike82@gmail.com>
2023-03-13 14:47:01 +0100

    update

--- admin-dashboard/src/axios.js
+++ admin-dashboard/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-    baseURL: "http://localhost:8080/api/",
+    baseURL: "https://bookingapi-jr8w.onrender.com/api/",
     withCredentials: true
 })
